6c1f6a152e45a833e8c5824dd66a2d960026e9f6
Diyor Olimov <diorliders@gmail.com>
2025-05-16 02:36:53 +0500

    almost there

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./Components/pages/home"
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    {/* <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, quisquam.</h1> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/pages/home.tsx
@@ -0,0 +1,9 @@
+import '../styles/home.css'
+
+function Navbar() {
+  return (
+    <h1>Lorem, ipsum dolor.</h1>
+  );
+}
+
+export default Navbar
