b6c5fe5f3f1ea1b5a89ccc7106582083e5ab7407
Diyor Olimov <diorliders@gmail.com>
2025-05-16 13:02:11 +0500

    herader layout

---
+++ src/Components/pages/header.tsx
@@ -0,0 +1,12 @@
+// src/components/Header.tsx
+import '../styles/header.css'; // Agar stillaringiz bo'lsa
+
+function Header() {
+  return (
+    <header>
+      <div className="logo">Sizning Loyiha Nomingiz</div>
+    </header>
+  );
+}
+
+export default Header;
