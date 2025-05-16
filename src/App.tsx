d4120ccf58ff0a208b34c7abc7bf96e47e33f9b4
Diyor Olimov <diorliders@gmail.com>
2025-05-16 14:44:15 +0500

    navbar + footer is ended

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // src/App.tsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './Components/pages/layout';
-import MainPage from './Components/pages/main_page';
+import MainPage from './Components/pages/main_home_page';
 import Contact from './Components/pages/contact';
 import Services from './Components/pages/services';
 import '../src/Components/styles/index.css';
