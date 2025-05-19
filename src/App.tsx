9f765ed3f1fca339524accd30769a131674acd8c
diorsolutions <diorliders@gmail.com>
2025-05-20 00:15:31 +0500

    npm successfully added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
+import './App.css';
 import Navbar from './modules/navbar/navbar';
 import Home from './pages/Home';
 import Services from './Components/services/services';
