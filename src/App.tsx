c5ebaca7bc3a3d85eb0390528f7d045e9858a501
diorsolutions <diorliders@gmail.com>
2025-05-19 16:46:33 +0500

    app.tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,16 @@ import Navbar from './modules/navbar/navbar';
 import Home from './pages/Home';
 import Services from './Components/services/services';
 import Contact from './Components/contact/contact';
-import Footer from './modules/footer';
+import Footer from './modules/footer/footer';
 // >>>>>>> d641eedb6d2de0f8cc14d229c56743350dfc99ff
 
 
 // til
 import './i18n'
-
 function App() {
   return (
     <BrowserRouter>
-    <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Components/contact/contact" element={<Contact />} />
